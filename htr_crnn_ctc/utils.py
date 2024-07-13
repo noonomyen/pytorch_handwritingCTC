bf4ac01feb24e8c03b71e0e6549325fea45ba1fb
noonomyen <nomyen@noonomyen.com>
2024-07-14 00:56:14 +0700

    Add util function for find best model from leven value

--- htr_crnn_ctc/utils.py
+++ htr_crnn_ctc/utils.py
@@ -1,3 +1,6 @@
+from os import listdir
+from typing import Optional
+
 from torch import Tensor
 
 import numpy as np
@@ -6,7 +9,8 @@ from htr_crnn_ctc.types import Sample, SampleImage, CharDict, DecodeMap
 
 __all__ = [
     "copy_sample",
-    "get_decode_map"
+    "get_decode_map",
+    "get_best_leven_value_model"
 ]
 
 def copy_sample(sample: Sample) -> Sample:
@@ -27,3 +31,15 @@ def copy_sample(sample: Sample) -> Sample:
 
 def get_decode_map(char_dict: CharDict) -> DecodeMap:
     return {idx: char for char, idx in char_dict.items()}
+
+def get_best_leven_value_model(path: Optional[str] = None) -> str:
+    l = [f for f in listdir(path) if f.endswith("_model.pth")]
+    assert len(l) != 0
+
+    best = (10e9, "")
+    for file in l:
+        leven = float(".".join(file.split("_", 2)[:2]))
+        if leven < best[0]:
+            best = (leven, file)
+
+    return best[1]
