b86d86d83cff69bc656a4fb2f2de337604406835
noonomyen <nomyen@noonomyen.com>
2024-07-19 13:03:32 +0700

    Update train log summary

--- utils/train-log-csv.py
+++ utils/train-log-csv.py
@@ -14,19 +14,19 @@ for idx, line in enumerate(data):
         split = line.split(" ")
         epoch = int(split[1][:-1])
 
-    if epoch < old_epoch:
-        fit += 1
+        if epoch < old_epoch:
+            fit += 1
 
-    old_epoch = epoch
-    sum_epoch += 1
+        old_epoch = epoch
+        sum_epoch += 1
 
-    outw.writerow((
-        fit,
-        sum_epoch,
-        float(split[4]),
-        float(split[8]),
-        float(split[11]),
-        float(split[14]),
-        float(split[17]),
-        float(split[21])
-    ))
+        outw.writerow((
+            fit,
+            sum_epoch,
+            float(split[4]),
+            float(split[8]),
+            float(split[11]),
+            float(split[14]),
+            float(split[17]),
+            float(split[21])
+        ))
